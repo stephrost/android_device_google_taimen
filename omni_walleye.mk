fd8c702ad3655c69c68db3523eb8c270f7787ec2
Stephen Frost <17633859+stephrost@users.noreply.github.com>
2018-10-02 13:01:59 -0400

    Update and rename aosp_walleye.mk to omni_walleye.mk

--- omni_walleye.mk
+++ omni_walleye.mk
@@ -18,23 +18,34 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-$(call inherit-product, device/google/walleye/device.mk)
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+# Inherit some commmon Omni stuff
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/google/muskie/device-walleye.mk)
 $(call inherit-product-if-exists, vendor/google_devices/muskie/proprietary/device-vendor-walleye.mk)
+
+ALLOW_MISSING_DEPENDENCIES := true
 
 PRODUCT_PACKAGES += \
     Dialer \
-    Launcher3 \
+    Launcher3QuickStep \
     WallpaperPicker
 
 PRODUCT_COPY_FILES += \
-    device/google/walleye/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    device/google/muskie/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_MANUFACTURER := Google
 PRODUCT_BRAND := Android
-PRODUCT_NAME := aosp_walleye
+PRODUCT_NAME := omni_walleye
 PRODUCT_DEVICE := walleye
-PRODUCT_MODEL := AOSP on walleye
+PRODUCT_MODEL := OMNI on walleye
