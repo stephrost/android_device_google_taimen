505c23cec773d307f2e3535f77112f145c765c24
Stephen Frost <17633859+stephrost@users.noreply.github.com>
2018-10-02 13:03:16 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,6 @@
 # limitations under the License.
 #
 
-add_lunch_combo du_walleye-userdebug
+add_lunch_combo omni_walleye-user
+add_lunch_combo omni_walleye-userdebug
+add_lunch_combo omni_walleye-eng
